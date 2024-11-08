ae54c149fc1e32093cc767749881466f90947284
kishan0779 <kishanmalviya1234567890@gmail.com>
2024-11-08 18:19:58 +0530

    a

---
+++ app/hook/usestate/countercomponent/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+function page() {
+    const increase = 1
+  return (
+    <div className="flex   ">
+      <div className="flex flex-col">
+        <h1>12</h1>
+        <div className="flex ">
+          <button type="button">+</button>
+          <button type="button">-</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default page;
