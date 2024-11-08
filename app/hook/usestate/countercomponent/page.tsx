d8e333549bec8a98b31c10ce44fd85a6768122e6
Kartik Malviya <87184927+Kartik-Malviya68@users.noreply.github.com>
2024-11-08 18:49:15 +0530

    add utility functions, button component, and Tailwind CSS configuration

--- app/hook/usestate/countercomponent/page.tsx
+++ app/hook/usestate/countercomponent/page.tsx
@@ -1,18 +1,9 @@
-import React from "react";
+import React from 'react'
 
 function page() {
-    const increase = 1
   return (
-    <div className="flex   ">
-      <div className="flex flex-col">
-        <h1>12</h1>
-        <div className="flex ">
-          <button type="button">+</button>
-          <button type="button">-</button>
-        </div>
-      </div>
-    </div>
-  );
+    <div>page</div>
+  )
 }
 
-export default page;
+export default page
